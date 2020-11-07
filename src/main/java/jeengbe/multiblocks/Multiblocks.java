9ab4a3045f529283cd735628f3b0d37532511915
jeengbe <jesper.engberg@gmx.at>
2020-11-07 16:56:07 +0100

    Plugin Base

---
+++ src/main/java/jeengbe/multiblocks/Multiblocks.java
@@ -0,0 +1,16 @@
+package jeengbe.multiblocks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.annotation.plugin.Description;
+import org.bukkit.plugin.java.annotation.plugin.Plugin;
+import org.bukkit.plugin.java.annotation.plugin.author.Author;
+
+@Plugin(name = "Multiblocks", version = "0.0.1-SNAPSHOT")
+@Description("Multiblock Structures")
+@Author("Jeengbe")
+public class Multiblocks extends JavaPlugin {
+  @Override
+  public void onEnable() {
+
+  }
+}
